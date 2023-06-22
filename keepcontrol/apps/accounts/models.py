19ffba6455722a1c1a26fd06d818d538081a4183
Ewerson Pulquerio <pulquerio.x@gmail.com>
2023-06-22 16:32:10 -0300

    Relação Episódios/Movies e Usuários

--- keepcontrol/apps/accounts/models.py
+++ keepcontrol/apps/accounts/models.py
@@ -2,6 +2,9 @@ import re
 from django.db import models
 from django.core import validators
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin, UserManager)
+from apps.core.models import Episode
+from apps.movies.models import Movie
+# from books.models import Book
 
 class User (AbstractBaseUser, PermissionsMixin):
     username = models.CharField('Nome de usuário', max_length=30, unique=True,
@@ -17,6 +20,12 @@ class User (AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField('Está ativo?', blank=True, default=True)
     is_staff = models.BooleanField('É da equipe', blank=True, default=False)
     date_joined = models.DateTimeField('Data de entrada', auto_now_add=True)
+    
+    #Conteúdo
+    episodes = models.ManyToManyField(Episode)
+    movies = models.ManyToManyField(Movie)
+    # chapters = models.ManyToManyField(Chapter)
+    # books = models.ManyToManyField(Book)
     
     objects = UserManager()
     
