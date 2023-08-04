4837c420d024c8d2632c050b3ef454dc8e93d4f2
Ewerson Pulquerio <pulquerio.x@gmail.com>
2023-08-03 22:53:53 -0300

    pagination movies

--- keepcontrol/apps/movies/views.py
+++ keepcontrol/apps/movies/views.py
@@ -3,25 +3,32 @@ from .models import Movie
 from apps.accounts.models import UserMovie
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 # Create your models here.
 
 def ListMovie(request):
     
     movies = Movie.objects.all().order_by('collection', '-year')
     
-    qtd_movies = len(movies)
-
     usuario = request.user
     
     for movie in movies:
         user_movie = movie.usermovie_set.filter(user=usuario.id).first()
         if user_movie and user_movie.date_watched:
-            movie.date_watched = user_movie.date_watched 
+            movie.date_watched = user_movie.date_watched
+    
+    movies_paginator = Paginator(movies, 20)
+    
+    page_num = request.GET.get('page')
+   
+    page = movies_paginator.get_page(page_num)
     
     template_name = 'movies.html'
     context = {
-        'movies': movies,
-        'qtd_movies': qtd_movies
+        'page': page,
+        #'qtd_movies': movies.count()
+        'qtd_movies': movies_paginator.count,
+        'qtd_pages': movies_paginator.num_pages
     }
 
     return render(request, template_name, context)
